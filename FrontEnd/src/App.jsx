8569885c420c0b4b475fed6ffe195c9c82192a64
Okkes <okkesakkurt46@gmail.com>
2023-10-11 09:59:35 +0300

    Route structure

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,18 +1,43 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import HomePage from '../Components/HomePage'
-
+import {BrowserRouter as Router, Switch, Route, Link,Routes} from 'react-router-dom';
+import Audience from './components/Audience/Audience';
+import Market from './components/Market/Market';
+import Online from './components/Online/Online';
+import Team from './components/Team/Team';
+import HomePage from './pages/HomePage';
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <HomePage/>
-    
-    </>
-  )
+      <div>
+          <BrowserRouter>
+        <Routes>
+          <Route path='/' exact element={<HomePage/>}/>
+                  <Route
+                      path='/audience'
+                      element={<Audience />}
+                  />
+                  <Route
+                      path='/market'
+                      element={<Market/>}
+                  />
+                  <Route
+                      path='/online'
+                      element={<Online/>}
+                  />
+                  <Route
+                      path='/tean'
+                      element={<Team/>}
+                  />
+                  <Route
+                      path='/audience'
+                      element={<Audience />}
+                  />
+              </Routes>
+          </BrowserRouter>
+      </div>
+  );
 }
 
 export default App
---
+++ FrontEnd/src/components/Audience/Audience.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Audience = () => {
+  return (
+    <div>Audience</div>
+  )
+}
+
+export default Audience
