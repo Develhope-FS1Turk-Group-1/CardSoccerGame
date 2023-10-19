a52ef0842798685aaba17581c882126b00db4f71
Alp Eren <erenalpturk@gmail.com>
2023-10-19 20:20:10 +0300

    header component create

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -7,9 +7,11 @@ import HomePage from './components/Home/Home';
 import LoginPage from './components/Login/Login';
 import RegisterPage from './components/Register/Register';
 import Dashboard from './components/Dashboard/Dashboard';
+import Header from '../src/layouts/Header/index';
 function App() {
     return (
         <div>
+            <Header />
             <Router>
                 <Routes>
                     <Route
