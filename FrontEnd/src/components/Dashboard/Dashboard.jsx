c2533bf2cbfe031e8e805bcc63c2e41d102c109b
Taha Ören <taha33oren@gmail.com>
2023-10-24 00:22:39 +0300

    Dashboard Page

--- FrontEnd/src/components/Dashboard/Dashboard.jsx
+++ FrontEnd/src/components/Dashboard/Dashboard.jsx
@@ -1,5 +1,8 @@
-import {useEffect, useState} from 'react';
-
+import { useEffect, useState } from 'react';
+import './DashboardStyle.css'
+import tactics from "../../../Assets/Dashboard/tactics.jpg"
+import playnow from "../../../Assets/Dashboard/playnow.jpg"
+import market from "../../../Assets/Dashboard/market.jpg"
 const Dashboard = () => {
   const [user, setUser] = useState();
 
@@ -11,14 +14,29 @@ const Dashboard = () => {
 
   }, [])
   console.log(user)
-
-
-
-    return (
-        <div>
-            <h1>Hoşgeldin {user}.</h1>
+  return (
+    <div>
+      <div className="DashboardBackground">
+        <div className="Dashboarddiv">
+          <div className="tacticsDiv">
+            <h1>Tactics</h1>
+            <span><h1>Formation</h1></span>
+            <img src={tactics} alt="" />
+          </div>
+          <div className="playDiv">
+            <h1>Play</h1>
+            <span><h1>Now</h1></span>
+            <img src={playnow} alt="" />
+          </div>
+          <div className="marketDiv">
+            <h1>Card</h1>
+            <span><h1>Market</h1></span>
+            <img src={market} alt="" />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
