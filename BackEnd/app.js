c9469d03989031d7b8de08f90540d7ab690672e5
Alp Eren <erenalpturk@gmail.com>
2023-10-26 11:21:40 +0300

    getAllPlayers endpoint create

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -22,68 +22,68 @@ app.use(cors());
 const pool = new Pool({ connectionString });
 let client;
 
-async function connectDB(){
-    client = await pool.connect();
-    console.log("Successfully connected to db!");
+async function connectDB() {
+  client = await pool.connect();
+  console.log("Successfully connected to db!");
 }
 
 
 app.listen(PORT, () => {
-    connectDB();
-    console.log(`Server is running on http://localhost:${PORT}`);
+  connectDB();
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 
 
 app.post('/register', async (req, res) => {
-    const { username, password, mail } = req.body;
+  const { username, password, mail } = req.body;
 
-    try {
-      const registrationDate = new Date(); // Assuming registration date is current date
+  try {
+    const registrationDate = new Date(); // Assuming registration date is current date
 
-      const query = `
+    const query = `
         INSERT INTO users (username, password, mail, registrationDate,money,xp)
         VALUES ($1, $2, $3, $4,0,0)
         RETURNING *
       `;
 
-      const result = await client.query(query, [username, password, mail, registrationDate]);
+    const result = await client.query(query, [username, password, mail, registrationDate]);
 
-      res.status(201).json({ message: 'User registered successfully', user: result.rows[0] });
-    } catch (error) {
-      console.error('Error registering user', error);
-      res.status(500).json({ message: 'Internal server error' });
-    }
-  });
+    res.status(201).json({ message: 'User registered successfully', user: result.rows[0] });
+  } catch (error) {
+    console.error('Error registering user', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
 
 
-  app.post('/login', async (req, res) => {
-    const { mail, password } = req.body;
-    console.log(mail , password);
-    try {
-      const query = `
+app.post('/login', async (req, res) => {
+  const { mail, password } = req.body;
+  console.log(mail, password);
+  try {
+    const query = `
         SELECT userId, username, money, xp
         FROM users
         WHERE mail = $1 AND password = $2
       `;
 
-      const result = await pool.query(query, [mail, password]);
+    const result = await pool.query(query, [mail, password]);
 
-      if (result.rows.length === 1) {
-        console.log((result.rows[0]));
-        const user = result.rows[0];
-        res.json({ userId: user.userid, username: user.username });
-      } else {
-        res.status(401).json({ message: 'Invalid credentials' });
-      }
-    } catch (error) {
-      console.error('Error logging in', error);
-      res.status(500).json({ message: 'Internal server error' });
+    if (result.rows.length === 1) {
+      console.log((result.rows[0]));
+      const user = result.rows[0];
+      res.json({ userId: user.userid, username: user.username });
+    } else {
+      res.status(401).json({ message: 'Invalid credentials' });
     }
-  });
+  } catch (error) {
+    console.error('Error logging in', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
 
 
-  app.get('/getUsername/:userId', async (req, res) => {
+app.get('/getUsername/:userId', async (req, res) => {
   const userId = req.params.userId;
 
   try {
@@ -109,31 +109,66 @@ app.post('/register', async (req, res) => {
 
 
 app.put('/updatePassword', async (req, res) => {
-    const { mail, oldPassword, newPassword } = req.body;
+  const { mail, oldPassword, newPassword } = req.body;
 
-    try {
-      const query = `
+  try {
+    const query = `
         UPDATE users
         SET password = $1
         WHERE mail = $2 AND password = $3
         RETURNING *
       `;
 
-      const result = await pool.query(query, [newPassword, mail, oldPassword]);
+    const result = await pool.query(query, [newPassword, mail, oldPassword]);
 
-      if (result.rows.length === 1) {
-        const updatedUser = result.rows[0];
-        res.json({ message: 'Password updated successfully', user: updatedUser });
-      } else {
-        res.status(401).json({ message: 'Invalid credentials' });
-      }
-    } catch (error) {
-      console.error('Error updating password', error);
-      res.status(500).json({ message: 'Internal server error' });
+    if (result.rows.length === 1) {
+      const updatedUser = result.rows[0];
+      res.json({ message: 'Password updated successfully', user: updatedUser });
+    } else {
+      res.status(401).json({ message: 'Invalid credentials' });
     }
-  });
+  } catch (error) {
+    console.error('Error updating password', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
 
+app.get('/players/:userID', async (req, res) => {
+  const userId = req.params.userId;
 
+  try {
+    const result = await pool.query(
+      `SELECT * FROM 
+      onlinePlayers JOIN basePlayers 
+      ON onlinePlayers.baseId = basePlayers.id
+         WHERE userID = $1`,
+      [userID]
+    );
+
+    res.json(result.rows);
+  } catch (error) {
+    console.error('Error executing query', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/formation/:userID', async (req, res) => {
+  const userId = req.params.userId;
+
+  try {
+    const result = await pool.query(
+      `SELECT * FROM formation
+         JOIN basePlayers ON onlinePlayers.baseId = basePlayers.id
+         WHERE userID = $1`,
+      [userID]
+    );
+
+    res.json(result.rows);
+  } catch (error) {
+    console.error('Error executing query', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 
 
