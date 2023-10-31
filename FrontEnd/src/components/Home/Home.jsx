ba81adf810176d19af299d07f4c49a1cec4118b9
Alp Eren <erenalpturk@gmail.com>
2023-10-31 15:57:41 +0300

    homepage button add

--- FrontEnd/src/components/Home/Home.jsx
+++ FrontEnd/src/components/Home/Home.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import {Link,useNavigate} from 'react-router-dom';
+
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
+      <button onClick={()=>{
+                        navigate('/login');
+                    }}>Login</button>
+                    <button onClick={()=>{
+                        navigate('/register');
+                    }}>register</button>
       </div>
   );
 }
