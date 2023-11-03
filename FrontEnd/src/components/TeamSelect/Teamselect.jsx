7dce95ee946ca67d817d1e3af4b15bf9f7168751
Alp Eren <erenalpturk@gmail.com>
2023-11-03 20:39:48 +0300

    teamselect css not completed

--- FrontEnd/src/components/TeamSelect/Teamselect.jsx
+++ FrontEnd/src/components/TeamSelect/Teamselect.jsx
@@ -3,44 +3,52 @@ import './Style.css'
 import arrow from "./arrow-right.svg"
 import stad from "./stad.png"
 import { useUserProvider } from '../../Contexts/UserContext';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from 'swiper/modules';
+
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 import axios from 'axios';
 
 
 const Teamselect = () => {
-	const{setMoney,money,setLevel,level,userId,setUserId} = useUserProvider();
-    const[leagues,setLeagues] = useState([]);
-    const[teams,setTeams] = useState([]);
+    const { setMoney, money, setLevel, level, userId, setUserId } = useUserProvider();
+    const [leagues, setLeagues] = useState([]);
+    const [teams, setTeams] = useState([]);
     const [selectedLeague, setSelectedLeague] = useState('');
     const [selectedTeam, setSelectedTeam] = useState('');
-  
+
     const handleLeagueChange = (event) => {
-      setSelectedLeague(event.target.value);
-      // Perform any additional actions based on the selected league if needed
+        setSelectedLeague(event.target.value);
+        // Perform any additional actions based on the selected league if needed
     };
-  
+
     const handleTeamChange = (event) => {
         console.log(event.target);
-      setSelectedTeam(event.target.value);
-      // Perform any additional actions based on the selected team if needed
+        setSelectedTeam(event.target.value);
+        // Perform any additional actions based on the selected team if needed
     };
 
     const navigate = useNavigate();
     useEffect(() => {
-        if(userId == 0)
+        if (userId == 0)
             navigate("/login");
 
-            axios
-			.get(`http://localhost:3050/play/getLeagues`)
-			.then((response) => {
-				console.log(response.data);
-				setLeagues(response.data);
-			})
-			.catch((error) => {
-				console.error('Error:', error);
-			});
-        
+        axios
+            .get(`http://localhost:3050/play/getLeagues`)
+            .then((response) => {
+                console.log(response.data);
+                setLeagues(response.data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+
 
     }, [])
 
@@ -48,7 +56,7 @@ const Teamselect = () => {
 
     useEffect(() => {
         axios
-            .get('http://localhost:3050/play/getTeams/'+selectedLeague)
+            .get('http://localhost:3050/play/getTeams/' + selectedLeague)
             .then((response) => {
                 console.log(response.data);
                 setTeams(response.data);
@@ -58,7 +66,117 @@ const Teamselect = () => {
             });
     }, [selectedLeague])
 
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
 
+    return (
+        <div>
+            <div className="DashboardBackground">
+                <div className="Dashboarddiv">
+                    <div onClick={handleLeagueChange}>
+                    <Swiper
+                        navigation={true}
+                        modules={[Pagination, Navigation]}
+                        className="mySwiper"
+                    >
+                        {leagues.map((league, index) => (
+                            <SwiperSlide key={index} value={league}>
+                                {league}
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                    </div>
+                    <Swiper
+                        navigation={true}
+                        modules={[Pagination, Navigation]}
+                        className="mySwiper"
+                    >
+                        {teams.map((team, index) => (
+                                    <SwiperSlide key={index} value={team.team_name}>
+                                        {team.team_name}
+                                    </SwiperSlide>
+                                ))}
+                    </Swiper>
+
+
+                    <div className='leaugeTeamSelect'>
+
+                        <label>
+                            Select League:
+                            <select value={selectedLeague} onChange={handleLeagueChange}>
+                                <option value="">--Select League--</option>
+                                {leagues.map((league, index) => (
+                                    <option key={index} value={league}>
+                                        {league}
+                                    </option>
+                                ))}
+                            </select>
+                        </label>
+
+
+                        <label>
+                            Select Team:
+                            <select value={selectedTeam} onChange={handleTeamChange}>
+                                <option value="">--Select Team--</option>
+                                {teams.map((team, index) => (
+                                    <option key={index} value={team.team_name}>
+                                        {team.team_name}
+                                    </option>
+                                ))}
+                            </select>
+                        </label>
+                    </div>
+                    <div className="ButtonClass">
+                        <button id='GrayButton'>BACK</button>
+                        <button id='GreenButton' onClick={() => {
+                            axios
+                                .post(`http://localhost:3050/play/playSingle/${selectedTeam}/${userId}`)
+                                .then((response) => {
+                                    console.log(response.data);
+                                })
+                                .catch((error) => {
+                                    console.error('Error:', error);
+                                });
+
+                        }}>START</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Teamselect;
+
+
+/*
+
+<div className="playDiv">
+                        <h1>Game <span>Day</span></h1>
+                        <div className="TeamSelect">
+                            <div className="TeamSide">
+                                <img id='leftbot' src={arrow} alt="" />
+                                <span className="dot"></span>
+                                <img src={arrow} alt="" />
+                            </div>
+                        </div>
+                        <div className="TeamSelect">
+                            <div className="TeamSide">
+                                <img id='leftbot' src={arrow} alt="" />
+                                <span className="dot"></span>
+                                <img src={arrow} alt="" />
+                            </div>
+                        </div>
+                        </div>
+
+*/
+
+/*
 
     return (
         <div>
@@ -122,26 +240,5 @@ const Teamselect = () => {
 };
 
 export default Teamselect;
-
-
-/*
-
-<div className="playDiv">
-                        <h1>Game <span>Day</span></h1>
-                        <div className="TeamSelect">
-                            <div className="TeamSide">
-                                <img id='leftbot' src={arrow} alt="" />
-                                <span className="dot"></span>
-                                <img src={arrow} alt="" />
-                            </div>
-                        </div>
-                        <div className="TeamSelect">
-                            <div className="TeamSide">
-                                <img id='leftbot' src={arrow} alt="" />
-                                <span className="dot"></span>
-                                <img src={arrow} alt="" />
-                            </div>
-                        </div>
-                        </div>
 
 */
