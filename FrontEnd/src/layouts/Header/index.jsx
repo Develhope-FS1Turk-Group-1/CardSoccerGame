5c755bc423ade8c91d7f8a07ecd39df0cd7bf703
omer can <omercansoyu@gmail.com>
2023-11-15 23:10:11 +0300

    level and money update after match result

--- FrontEnd/src/layouts/Header/index.jsx
+++ FrontEnd/src/layouts/Header/index.jsx
@@ -1,56 +1,44 @@
-import { useEffect, useState } from 'react';
-import './header.css';
-import coinImage from '../../assets/coin.png'
-import soccerBallImage from '../../assets/soccerBallImage.png'
-import logOutIcon from '../../assets/logOutIcon.png'
-import { UserProvider, useUserProvider } from '../../Contexts/UserContext';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-
+import { useEffect, useState } from "react";
+import "./header.css";
+import coinImage from "../../assets/coin.png";
+import soccerBallImage from "../../assets/soccerBallImage.png";
+import logOutIcon from "../../assets/logOutIcon.png";
+import { UserProvider, useUserProvider } from "../../Contexts/UserContext";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Header = () => {
-    const { setMoney, money, setLevel, level, userId, setUserId } = useUserProvider();
+  const { setMoney, money, setLevel, level, userId, setUserId } =
+    useUserProvider();
 
-
-//     useEffect(() => {
-//      axios
-// 			.get(`http://localhost:3050/player/getMoney/${userId}`)
-// 			.then((response) => {
-//                 if (response.data) {
-// 					setMoney(response.data.money);
-// 				}
-// 			})
-// 			.catch((error) => {
-// 				console.log(error);
-// 			});
-//    }, [])
-
-    const navigate = useNavigate();
-    return (
-        <div className='headerAllContainer'>
-            <div className='headerSmallContainer'>
-                <div className='leftSide'>
-                    <div className='levelContainer'>
-                        <h3>Level:{level}</h3>
-                    </div>
-                    <div className='moneyContainer'>
-                        <img id='headerCoinImage' src={coinImage} alt="" />
-                        <div className='moneyValue'>{money}</div>
-                    </div>
-                </div>
-                <div className='rightSide' onClick={()=>{
-                    console.log("Log Out")
-                    setUserId(0);
-                    navigate('/login');
-                }}>
-
-                    <img id='soccerImage' src={soccerBallImage} alt="" />
-                    Log Out
-                    <img id='headerLogOutImage' src={logOutIcon} alt="" />
-                </div>
-            </div>
+  const navigate = useNavigate();
+  return (
+    <div className="headerAllContainer">
+      <div className="headerSmallContainer">
+        <div className="leftSide">
+          <div className="levelContainer">
+            <h3>Level: {Math.floor(level / 1000)}</h3>
+          </div>
+          <div className="moneyContainer">
+            <img id="headerCoinImage" src={coinImage} alt="" />
+            <div className="moneyValue">{money}</div>
+          </div>
         </div>
-    );
+        <div
+          className="rightSide"
+          onClick={() => {
+            console.log("Log Out");
+            setUserId(0);
+            navigate("/login");
+          }}
+        >
+          <img id="soccerImage" src={soccerBallImage} alt="" />
+          Log Out
+          <img id="headerLogOutImage" src={logOutIcon} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
